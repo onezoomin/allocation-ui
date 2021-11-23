99f5c3f8f1857c8641cccfc9fceb4f47ee5a43f4
gotjoshua <gitlab@begreen.nu>
2021-11-23 12:08:55 +0000

    wip more allocation mockup with versions

--- src/Data/AllocationEngine.ts
+++ src/Data/AllocationEngine.ts
@@ -4,10 +4,12 @@ const mockRecipientMap: Map<string, RecipientWeighted> = new Map(initialRecipien
 class DivvyAddress {
   recipientMap: Map<string, RecipientWeighted>
   owner: string
+  publicAddress: string
 
   constructor (newMap = mockRecipientMap, caller) {
     this.recipientMap = newMap
     this.owner = caller
+    this.publicAddress = determinePublicAddress()
   }
 
   setCurrentRecipientMap = (newMap: Map<string, RecipientWeighted>, caller = 'addressThatCalledTheContract'): void => {
@@ -20,57 +22,63 @@ class DivvyAddress {
     // return the current tally
     return this.recipientMap
   }
+
+  doDistribution = (totalAmount): void => {
+    // Distribute according to tally
+    this.getCurrentRecipientMap().forEach((eachRecip: RecipientWeighted) => {
+      // doSend(eachRecip.recipient.address, eachRecip.value*totalAmount)
+    })
+  }
 }
 
-const authorizedWhitelisters = ['addressThatCalledTheContract']
-const mockWhitelist: string[] = initialRecipients.map((r: Recipient) => r.recipient.address)
-const whitelistRecipientAddress = (newAddressToWhitelist: string, caller = 'addressThatCalledTheContract') => {
+class DivvyAddress2 extends DivvyAddress {
+  authorizedWhitelisters = ['addressThatCalledTheContract']
+  mockWhitelist: string[] = initialRecipients.map((r: Recipient) => r.recipient.address)
+
+  whitelistRecipientAddress = (newAddressToWhitelist: string, caller = 'addressThatCalledTheContract') => {
   // Check for authority/permission
-  if (!authorizedWhitelisters.includes(caller)) throw new Error('unauthorized attempt to whitelist an address')
+    if (!this.authorizedWhitelisters.includes(caller)) throw new Error('unauthorized attempt to whitelist an address')
 
-  // Grant whitelist authority to new address (Add to list of addresses that can add whitelist addresses)
-  mockWhitelist.push(newAddressToWhitelist)
-}
-
-const castWeightedVote = (recipientArray: Recipient[], weight: number) => {
-  // Ensure all recipients are whitelisted
-  for (const eachRecip of recipientArray) {
-    if (!mockWhitelist.includes(eachRecip.recipient.address)) throw new Error('unknown recipient address - vote is discarded and voting tokens returned')
+    // Grant whitelist authority to new address (Add to list of addresses that can add whitelist addresses)
+    this.mockWhitelist.push(newAddressToWhitelist)
   }
 
-  // Update running tally
-  const tally = getCurrentRecipientMap()
-  for (const eachRecip of recipientArray) {
-    console.log(weight, eachRecip.short)
-    const recipRef = (tally.get(eachRecip.recipient.address))
-    if (!recipRef) return
-    const newWeight = recipRef.weight + weight
-    recipRef.value = ((recipRef.weight * recipRef.value) + (weight * eachRecip.value)) / newWeight
-    recipRef.weight = newWeight
+  castWeightedVote = (recipientArray: RecipientWeighted[], weight: number) => {
+    // Ensure all recipients are whitelisted
+    for (const eachRecip of recipientArray) {
+      if (!this.mockWhitelist.includes(eachRecip.recipient.address)) throw new Error('unknown recipient address - vote is discarded and voting tokens returned')
+    }
+
+    // Update running tally
+    const tally = this.getCurrentRecipientMap()
+    for (const eachRecip of recipientArray) {
+      console.log(weight, eachRecip.short)
+      const recipRef = (tally.get(eachRecip.recipient.address))
+      if (!recipRef) return
+      const newWeight = +recipRef.weight + weight
+      recipRef.value = ((recipRef.weight * recipRef.value) + (weight * eachRecip.value)) / newWeight
+      recipRef.weight = newWeight
+    }
+
+    // TODO consider if we should Commit each vote record as an individual message/tx
+    const voteRecord = recipientArray
+
+    this.setCurrentRecipientMap(tally)
   }
 
-  // Commit vote record
-  const voteRecord = {
-    thisVote: recipientArray,
+  selfRemoval = (signedMessage, caller): void => {
+    // a recipient should be able to remove themselves
+    if (signedMessage.isSignedby(caller)) {
+      // remove
 
+      // reallocate / rebalance percentages
+    } else {
+      // throw
+    }
   }
-  setCurrentRecipientMap(tally)
-}
 
-const selfRemoval = (signedMessage, caller): void => {
-  // a recipient should be able to remove themselves
-  if (signedMessage.isSignedby(caller)) {
-    // remove
-
-    // reallocate / rebalance percentages
-  } else {
-    // throw
+  validateTally = (): boolean => {
+    // Retrace all vote records and recalculate tally
+    return false || true
   }
-}
-const triggerDistribution = (): void => {
-  // Distribute according to tally
-}
-const validateTally = (): boolean => {
-  // Retrace all vote records and recalculate tally
-  return false || true
 }
