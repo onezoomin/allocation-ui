18487f4723a9682b9e3806cac56c6d7a715884af
gotjoshua <gitlab@begreen.nu>
2021-11-23 11:51:33 +0000

    wip allocation engine mockup

--- src/Data/AllocationEngine.ts
+++ src/Data/AllocationEngine.ts
@@ -1,12 +1,36 @@
 import { initialRecipients, Recipient, RecipientWeighted } from './../Model/Allocations'
-const mockWhitelist: string[] = [
-  'regen1xq3aq5ctyj3knu32usn66df9a67n0sf7usqse1',
-  'regen1xq3aq5ctyj3knu32usn66df9a67n0sf7usqfl1',
-  'regen1xq3aq5ctyj3knu32usn66df9a67n0sf7usqfl2',
-  'regen1xq3aq5ctyj3knu32usn66df9a67n0sf7usqfr1',
-  'regen1xq3aq5ctyj3knu32usn66df9a67n0sf7usqfr2',
-]
-const runningTally = new Map(initialRecipients.map((r: Recipient) => [r.recipient.address, new RecipientWeighted(r)]))
+
+const mockRecipientMap: Map<string, RecipientWeighted> = new Map(initialRecipients.map((r: Recipient) => [r.recipient.address, new RecipientWeighted(r)]))
+class DivvyAddress {
+  recipientMap: Map<string, RecipientWeighted>
+  owner: string
+
+  constructor (newMap = mockRecipientMap, caller) {
+    this.recipientMap = newMap
+    this.owner = caller
+  }
+
+  setCurrentRecipientMap = (newMap: Map<string, RecipientWeighted>, caller = 'addressThatCalledTheContract'): void => {
+    if (caller !== this.owner) throw new Error('unauthorized attempt to setCurrentRecipientMap')
+    // set the current tally
+    this.recipientMap = newMap
+  }
+
+  getCurrentRecipientMap = (): Map<string, RecipientWeighted> => {
+    // return the current tally
+    return this.recipientMap
+  }
+}
+
+const authorizedWhitelisters = ['addressThatCalledTheContract']
+const mockWhitelist: string[] = initialRecipients.map((r: Recipient) => r.recipient.address)
+const whitelistRecipientAddress = (newAddressToWhitelist: string, caller = 'addressThatCalledTheContract') => {
+  // Check for authority/permission
+  if (!authorizedWhitelisters.includes(caller)) throw new Error('unauthorized attempt to whitelist an address')
+
+  // Grant whitelist authority to new address (Add to list of addresses that can add whitelist addresses)
+  mockWhitelist.push(newAddressToWhitelist)
+}
 
 const castWeightedVote = (recipientArray: Recipient[], weight: number) => {
   // Ensure all recipients are whitelisted
@@ -15,7 +39,7 @@ const castWeightedVote = (recipientArray: Recipient[], weight: number) => {
   }
 
   // Update running tally
-  const tally = getCurrentTally()
+  const tally = getCurrentRecipientMap()
   for (const eachRecip of recipientArray) {
     console.log(weight, eachRecip.short)
     const recipRef = (tally.get(eachRecip.recipient.address))
@@ -28,22 +52,21 @@ const castWeightedVote = (recipientArray: Recipient[], weight: number) => {
   // Commit vote record
   const voteRecord = {
     thisVote: recipientArray,
-    runningTally: tally, // TODO decide if this is part of the same tx / voteRecord or separate
+
+  }
+  setCurrentRecipientMap(tally)
+}
+
+const selfRemoval = (signedMessage, caller): void => {
+  // a recipient should be able to remove themselves
+  if (signedMessage.isSignedby(caller)) {
+    // remove
+
+    // reallocate / rebalance percentages
+  } else {
+    // throw
   }
 }
-const getCurrentTally = (): Map<string, RecipientWeighted> => {
-  // return the current tally
-  return runningTally
-}
-
-const authorizedWhitelisters = ['addressThatCalledTheContract']
-const whitelistRecipientAddress = (newAddressToWhitelist: string, caller = 'addressThatCalledTheContract') => {
-  // Check for authority/permission
-  if (!authorizedWhitelisters.includes(caller)) throw new Error('unauthorized attempt to whitelist an address')
-  // Grant whitelist authority to new address (Add to list of addresses that can add whitelist addresses)
-  mockWhitelist.push(newAddressToWhitelist)
-}
-
 const triggerDistribution = (): void => {
   // Distribute according to tally
 }
